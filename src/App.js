750136843c4ece676d9dbf6a17a3f06ebda41ca5
J, Gopi <gopi.j2@unisys.com>
2022-03-27 22:59:08 +0530

    - Initial commit to React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Create React App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
