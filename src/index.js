0416eed5440d0a7856fd19c5b31f042cb4102402
J, Gopi <gopi.j2@unisys.com>
2022-03-30 23:32:46 +0530

    - Started from basic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+function Title() {
+  return <div className='title'>The Title</div>;
+}
+
+function Author() {
+  return <div className='author'>The Author</div>;
+}
+
+function Stats() {
+  return (
+    <ul className='stats'>
+      <li className='rating'>"5 start"</li>
+      <li className='isbn'>12-3098757-098</li>
+    </ul>
+  );
+}
+
+function Greeting(props) {
+  return (
+    <div>
+      {(props.userName && `Hello ${props.userName}`) || `Not Logged in`}
+    </div>
+  );
+}
+
+function Book() {
+  return (
+    <div className='book'>
+      <Greeting userName='Gopi' />
+      <Title />
+      <Author />
+      <Stats />
+    </div>
+  );
+}
+
+ReactDom.render(<Book />, document.querySelector('#root'));
